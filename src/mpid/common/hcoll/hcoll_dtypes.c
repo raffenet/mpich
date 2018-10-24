fce042a8fe54d9fc8cca786dfcd38b380c8bc60b
Ken Raffenetti <raffenet@mcs.anl.gov>
2018-10-24 13:11:59 -0500

    hcoll: move some dtype handling to .c file
    Avoid a circular header include situation that results in undefined
    symbols.

--- src/mpid/common/hcoll/hcoll_dtypes.c
+++ src/mpid/common/hcoll/hcoll_dtypes.c
@@ -5,6 +5,99 @@
 extern int hcoll_initialized;
 extern int hcoll_enable;
 
+static dte_data_representation_t mpi_predefined_derived_2_hcoll(MPI_Datatype datatype)
+{
+    MPI_Aint size;
+
+    switch (datatype) {
+        case MPI_FLOAT_INT:
+            return DTE_FLOAT_INT;
+        case MPI_DOUBLE_INT:
+            return DTE_DOUBLE_INT;
+        case MPI_LONG_INT:
+            return DTE_LONG_INT;
+        case MPI_SHORT_INT:
+            return DTE_SHORT_INT;
+        case MPI_LONG_DOUBLE_INT:
+            return DTE_LONG_DOUBLE_INT;
+        case MPI_2INT:
+            return DTE_2INT;
+#ifdef HAVE_FORTRAN_BINDING
+#if HCOLL_API >= HCOLL_VERSION(3,7)
+        case MPI_2INTEGER:
+            MPIR_Datatype_get_size_macro(datatype, size);
+            switch (size) {
+                case 4:
+                    return DTE_2INT;
+                case 8:
+                    return DTE_2INT64;
+                default:
+                    return DTE_ZERO;
+            }
+        case MPI_2REAL:
+            MPIR_Datatype_get_size_macro(datatype, size);
+            switch (size) {
+                case 4:
+                    return DTE_2FLOAT32;
+                case 8:
+                    return DTE_2FLOAT64;
+                default:
+                    return DTE_ZERO;
+            }
+        case MPI_2DOUBLE_PRECISION:
+            MPIR_Datatype_get_size_macro(datatype, size);
+            switch (size) {
+                case 4:
+                    return DTE_2FLOAT32;
+                case 8:
+                    return DTE_2FLOAT64;
+                default:
+                    return DTE_ZERO;
+            }
+#endif
+#endif
+        default:
+            break;
+    }
+    return DTE_ZERO;
+}
+
+dte_data_representation_t
+mpi_dtype_2_hcoll_dtype(MPI_Datatype datatype, int count, const int mode)
+{
+    dte_data_representation_t dte_data_rep = DTE_ZERO;
+
+    if (HANDLE_GET_KIND((datatype)) == HANDLE_KIND_BUILTIN) {
+        /* Built-in type */
+        dte_data_rep = datatype_2_dte_data_rep(datatype);
+    }
+#if HCOLL_API >= HCOLL_VERSION(3,6)
+    else if (TRY_FIND_DERIVED == mode) {
+
+        /* Check for predefined derived types */
+        dte_data_rep = mpi_predefined_derived_2_hcoll(datatype);
+        if (HCOL_DTE_IS_ZERO(dte_data_rep)) {
+            MPIR_Datatype *dt_ptr;
+
+            /* Must be a non-predefined derived mapping, get it */
+            MPIR_Datatype_get_ptr(datatype, dt_ptr);
+            dte_data_rep = (dte_data_representation_t) dt_ptr->dev.hcoll_datatype;
+        }
+    }
+#endif
+
+    /* We always fall back, don't even think about forcing it! */
+    /* XXX Fix me
+     * if (HCOL_DTE_IS_ZERO(dte_data_rep) && TRY_FIND_DERIVED == mode
+     * && !mca_coll_hcoll_component.hcoll_datatype_fallback) {
+     * dte_data_rep = DTE_ZERO;
+     * dte_data_rep.rep.in_line_rep.data_handle.in_line.in_line = 0;
+     * dte_data_rep.rep.in_line_rep.data_handle.pointer_to_handle = (uint64_t) &datatype;
+     * }
+     */
+    return dte_data_rep;
+}
+
 /* This will only get called once */
 int hcoll_type_commit_hook(MPIR_Datatype * dtype_p)
 {
--- src/mpid/common/hcoll/hcoll_dtypes.h
+++ src/mpid/common/hcoll/hcoll_dtypes.h
@@ -53,63 +53,6 @@ static dte_data_representation_t datatype_2_dte_data_rep(MPI_Datatype datatype)
     }
 }
 
-static inline dte_data_representation_t mpi_predefined_derived_2_hcoll(MPI_Datatype datatype)
-{
-    MPI_Aint size;
-
-    switch (datatype) {
-        case MPI_FLOAT_INT:
-            return DTE_FLOAT_INT;
-        case MPI_DOUBLE_INT:
-            return DTE_DOUBLE_INT;
-        case MPI_LONG_INT:
-            return DTE_LONG_INT;
-        case MPI_SHORT_INT:
-            return DTE_SHORT_INT;
-        case MPI_LONG_DOUBLE_INT:
-            return DTE_LONG_DOUBLE_INT;
-        case MPI_2INT:
-            return DTE_2INT;
-#ifdef HAVE_FORTRAN_BINDING
-#if HCOLL_API >= HCOLL_VERSION(3,7)
-        case MPI_2INTEGER:
-            MPIR_Datatype_get_size_macro(datatype, size);
-            switch (size) {
-                case 4:
-                    return DTE_2INT;
-                case 8:
-                    return DTE_2INT64;
-                default:
-                    return DTE_ZERO;
-            }
-        case MPI_2REAL:
-            MPIR_Datatype_get_size_macro(datatype, size);
-            switch (size) {
-                case 4:
-                    return DTE_2FLOAT32;
-                case 8:
-                    return DTE_2FLOAT64;
-                default:
-                    return DTE_ZERO;
-            }
-        case MPI_2DOUBLE_PRECISION:
-            MPIR_Datatype_get_size_macro(datatype, size);
-            switch (size) {
-                case 4:
-                    return DTE_2FLOAT32;
-                case 8:
-                    return DTE_2FLOAT64;
-                default:
-                    return DTE_ZERO;
-            }
-#endif
-#endif
-        default:
-            break;
-    }
-    return DTE_ZERO;
-}
-
 // #if HCOLL_API => HCOLL_VERSION(3,6)
 // static inline
 // int hcoll_map_derived_type(MPI_Datatype *dtype, dte_data_representation *new_dte)
@@ -132,41 +75,7 @@ static inline dte_data_representation_t mpi_predefined_derived_2_hcoll(MPI_Datat
 // }
 // #endif
 
-static dte_data_representation_t
-mpi_dtype_2_hcoll_dtype(MPI_Datatype datatype, int count, const int mode)
-{
-    dte_data_representation_t dte_data_rep = DTE_ZERO;
-
-    if (HANDLE_GET_KIND((datatype)) == HANDLE_KIND_BUILTIN) {
-        /* Built-in type */
-        dte_data_rep = datatype_2_dte_data_rep(datatype);
-    }
-#if HCOLL_API >= HCOLL_VERSION(3,6)
-    else if (TRY_FIND_DERIVED == mode) {
-
-        /* Check for predefined derived types */
-        dte_data_rep = mpi_predefined_derived_2_hcoll(datatype);
-        if (HCOL_DTE_IS_ZERO(dte_data_rep)) {
-            MPIR_Datatype *dt_ptr;
-
-            /* Must be a non-predefined derived mapping, get it */
-            MPIR_Datatype_get_ptr(datatype, dt_ptr);
-            dte_data_rep = (dte_data_representation_t) dt_ptr->dev.hcoll_datatype;
-        }
-    }
-#endif
-
-    /* We always fall back, don't even think about forcing it! */
-    /* XXX Fix me
-     * if (HCOL_DTE_IS_ZERO(dte_data_rep) && TRY_FIND_DERIVED == mode
-     * && !mca_coll_hcoll_component.hcoll_datatype_fallback) {
-     * dte_data_rep = DTE_ZERO;
-     * dte_data_rep.rep.in_line_rep.data_handle.in_line.in_line = 0;
-     * dte_data_rep.rep.in_line_rep.data_handle.pointer_to_handle = (uint64_t) &datatype;
-     * }
-     */
-    return dte_data_rep;
-}
+dte_data_representation_t mpi_dtype_2_hcoll_dtype(MPI_Datatype datatype, int count, const int mode);
 
 static hcoll_dte_op_t *mpi_op_2_dte_op(MPI_Op op)
 {
