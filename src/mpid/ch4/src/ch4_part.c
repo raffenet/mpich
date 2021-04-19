8557f6373900ed23c7246aefad2b718c0e34bd85
Ken Raffenetti <raffenet@mcs.anl.gov>
2021-04-19 14:25:50 -0500

    ch4: Add hooks for partitioned communication initialization
    Instead of making NM and SHM responsible for calling the generic
    partitioned communciation initialization, put that in the ch4 layer
    and add hooks. This way we always use the generic code for the
    matching step, while still leaving opportunity for communication
    optimization at the lower layers.

--- src/mpid/ch4/src/ch4_part.c
+++ src/mpid/ch4/src/ch4_part.c
@@ -15,16 +15,22 @@ int MPID_Psend_init(void *buf, int partitions, MPI_Aint count,
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPID_PSEND_INIT);
 
     MPIDI_av_entry_t *av = MPIDIU_comm_rank_to_av(comm, dest);
+    int is_local = MPIDI_av_is_local(av);
+
+    mpi_errno =
+        MPIDIG_mpi_psend_init(buf, partitions, count, datatype, dest, tag, comm, info, is_local,
+                              request);
+    MPIR_ERR_CHECK(mpi_errno);
 
 #ifndef MPIDI_CH4_DIRECT_NETMOD
-    if (MPIDI_av_is_local(av)) {
-        mpi_errno = MPIDI_SHM_mpi_psend_init(buf, partitions, count, datatype, dest, tag,
-                                             comm, info, av, request);
+    if (is_local) {
+        mpi_errno = MPIDI_SHM_mpi_psend_init_hook(buf, partitions, count, datatype, dest, tag,
+                                                  comm, info, av, request);
     } else
 #endif
     {
-        mpi_errno = MPIDI_NM_mpi_psend_init(buf, partitions, count, datatype, dest, tag,
-                                            comm, info, av, request);
+        mpi_errno = MPIDI_NM_mpi_psend_init_hook(buf, partitions, count, datatype, dest, tag,
+                                                 comm, info, av, request);
     }
 
   fn_exit:
@@ -44,16 +50,22 @@ int MPID_Precv_init(void *buf, int partitions, MPI_Aint count,
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPID_PRECV_INIT);
 
     MPIDI_av_entry_t *av = MPIDIU_comm_rank_to_av(comm, source);
+    int is_local = MPIDI_av_is_local(av);
+
+    mpi_errno =
+        MPIDIG_mpi_precv_init(buf, partitions, count, datatype, source, tag, comm, info, is_local,
+                              request);
+    MPIR_ERR_CHECK(mpi_errno);
 
 #ifndef MPIDI_CH4_DIRECT_NETMOD
-    if (MPIDI_av_is_local(av)) {
-        mpi_errno = MPIDI_SHM_mpi_precv_init(buf, partitions, count, datatype,
-                                             source, tag, comm, info, av, request);
+    if (is_local) {
+        mpi_errno = MPIDI_SHM_mpi_precv_init_hook(buf, partitions, count, datatype,
+                                                  source, tag, comm, info, av, request);
     } else
 #endif
     {
-        mpi_errno = MPIDI_NM_mpi_precv_init(buf, partitions, count, datatype,
-                                            source, tag, comm, info, av, request);
+        mpi_errno = MPIDI_NM_mpi_precv_init_hook(buf, partitions, count, datatype,
+                                                 source, tag, comm, info, av, request);
     }
 
   fn_exit:
